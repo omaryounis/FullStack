603bf3ac3308206ca57ea49d856f1a46697c1385
omar rabie abd elmaksoud <omarrabie83@gmail.com>
2020-10-24 00:55:44 +0200

    install ngx bootstrap

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  
   title = 'Dating App';
   users:any;
   constructor(private _http:HttpClient) {
   }
   ngOnInit(): void {
+    this.getUser();
+  }
+  getUser(){
     this._http.get('https://localhost:5001/api/users').subscribe(response=>{
       this.users=response;
     },error=>{
