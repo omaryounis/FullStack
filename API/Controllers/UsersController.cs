fc2903beb2936a9a95d2d735b1acbd700c7d162a
omar rabie abd elmaksoud <omarrabie83@gmail.com>
2020-10-23 15:10:50 +0200

    First Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+           return await _dataContext.Users.ToListAsync();
+        }
+
+        [HttpGet("{Id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int Id)
+        {
+            return await _dataContext.Users.FindAsync(Id);
+        }
+    }
+}
